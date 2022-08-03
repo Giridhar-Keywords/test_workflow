c8eb1cec872821e8f635bd05227c35ee69e19c15
Giridhar Raut <GRaut@keywordsstudios.com>
2022-08-03 19:17:39 +0530

    Text change001

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'hello world'),
+      home: const MyHomePage(title: 'continuous Integration'),
     );
   }
 }
